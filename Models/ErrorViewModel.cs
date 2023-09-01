356d95c11ad2ff879774ba19afa1cbff9fc019e6
Will <doggy.huang@gmail.com>
2023-09-01 09:14:44 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspNetCoreMvcDefaultHtmlEncoder.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
